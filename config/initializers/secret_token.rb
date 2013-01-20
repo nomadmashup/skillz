98915d1aecf703140c614a093e3aa038de854e29
James Trask <pair+james@pivotallabs.com>
2013-01-20 13:30:53 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillz::Application.config.secret_token = 'd66ecc1351a0dccf7f284f93bf3cbe7e9c68e51dfedb86d7120eb70c52c88ca6ee54d3d4bf36c2f18dd9814014967188493d42c9488bb18da622e61bc00b1305'
