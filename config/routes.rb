474df00aa3ff5dffc1a5dda5e9e4be7c4e415be2
James Trask <pair+james@pivotallabs.com>
2013-01-20 13:46:03 -0800

    initial skills controller, hello world

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Skillz::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :skills
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
