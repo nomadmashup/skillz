bdeae7e598dc07eb652eced9ca56204cdceb3cef
James Trask <pair+james@pivotallabs.com>
2013-01-29 08:50:22 -0800

    updated list of skills; fixed csv seeding

--- app/models/Skill.rb
+++ app/models/Skill.rb
@@ -8,38 +8,28 @@ class Skill < ActiveRecord::Base
 
   attr_accessible :code, :label, :description, :parent, :sort_order
 
+  TOP_PARENT_CODE = "top"
+
   def self.seed(overwrite = false)
-
-    path = Rails.root.join('db','seeds','skills.yml')
-    File.open(path) do |file|
-      YAML.load_documents(file) do |doc|
-        doc.keys.each do |key|
-          attributes = doc[key].merge(code: key)
-          record = find_or_create_by_code key, attributes
-          record.update_attributes! attributes if overwrite
-        end
-      end
-    end
-
-    path = Rails.root.join('db','seeds','skills.csv')
+    path = Rails.root.join('db','seeds','skillz.csv')
     CSV.parse(File.read(path), headers: true) do |row|
+      next if TOP_PARENT_CODE == row["code"]
       attributes = {
-        code: row["code"],
-        label: row["label"],
-        parent: row["parent"].present? ? row["parent"] : nil,
-        description: row["description"].present? ? row["description"] : nil,
-        sort_order: row["sort_order"]
+        code: row["code"].present? ? row["code"].strip : nil,
+        label: row["label"].present? ? row["label"].strip : nil,
+        parent: row["parent"].present? ? row["parent"].strip : nil,
+        description: row["description"].present? ? row["description"].strip : nil,
+        sort_order: row["sort_order"].present? ? row["sort_order"].strip : nil
       }
       record = find_or_create_by_code attributes[:code], attributes
       record.update_attributes! attributes if overwrite
     end
-
   end
 
   def depth
     level = 0
     skill = self
-    until skill.parent.blank? do
+    until TOP_PARENT_CODE == skill.parent do
       level += 1
       skill = Skill.find_by_code(skill.parent)
     end
@@ -47,13 +37,13 @@ class Skill < ActiveRecord::Base
   end
 
   def top_parent
-    parents.first.code rescue nil
+    parents.first.code rescue TOP_PARENT_CODE
   end
 
   def parents
     list = []
     skill = self
-    until skill.parent.blank? do
+    until TOP_PARENT_CODE == skill.parent do
       skill = Skill.find_by_code(skill.parent)
       list.unshift(skill)
     end
